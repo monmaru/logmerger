489a997c72cf05dce78fce79fd4195e3621e65b0
Yoshitomo Minami <minami530@gmail.com>
2020-03-20 11:39:06 +0900

    Add sort condition

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,22 @@ use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::io::{self, BufRead, BufReader};
+use std::path::Path;
 
 #[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
 struct Line {
     time: NaiveDateTime,
     content: String,
+    priority: u8,
 }
 
 impl Line {
-    pub fn new(time: NaiveDateTime, content: String) -> Self {
-        Line { time, content }
+    pub fn new(time: NaiveDateTime, content: String, priority: u8) -> Self {
+        Line {
+            time,
+            content,
+            priority,
+        }
     }
 }
 
@@ -23,38 +29,40 @@ const LINE_ENDING: &'static str = "\r\n";
 #[cfg(not(windows))]
 const LINE_ENDING: &'static str = "\n";
 
-fn main() -> Result<(), io::Error> {
-    let args: Vec<String> = env::args().collect();
-    let mut base = read_log_file(&args[1])?;
-    let target = read_log_file(&args[2])?;
-    base.extend(target);
-    base.sort_by(|a, b| a.time.cmp(&b.time));
-
-    let mut f = File::create("foo.txt")?;
-    for l in base {
-        f.write(l.content.as_bytes())?;
-        f.write(LINE_ENDING.as_bytes())?;
-    }
-    Ok(())
-}
-
-fn read_log_file(path: &str) -> Result<Vec<Line>, io::Error> {
+fn read_log_file(path: &str, priority: u8) -> Result<Vec<Line>, io::Error> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
     let mut ret: Vec<Line> = Vec::new();
-    for line in reader.lines() {
-        let line = line?;
-        let v: Vec<&str> = line.split('|').map(|s| s.trim()).collect();
+    for content in reader.lines() {
+        let content = content?;
+        let v: Vec<&str> = content.split('|').map(|s| s.trim()).collect();
         match NaiveDateTime::parse_from_str(v[0], "%F %H:%M:%S,%3f") {
             Ok(dt) => {
-                ret.push(Line::new(dt, line));
+                ret.push(Line::new(dt, content, priority));
             }
-            Err(_err) => {
+            Err(_) => {
                 if let Some(last) = ret.last_mut() {
-                    last.content += &(LINE_ENDING.to_owned() + &line);
+                    last.content += &(LINE_ENDING.to_owned() + &content);
                 }
             }
         }
     }
     Ok(ret)
+}
+
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = env::args().collect();
+    let mut base_log = read_log_file(&args[1], 1)?;
+    let target_log = read_log_file(&args[2], 2)?;
+    base_log.extend(target_log);
+    base_log.sort_by(|a, b| a.time.cmp(&b.time).then(a.priority.cmp(&b.priority)));
+
+    let file_stem = Path::new(&args[1]).file_stem().unwrap();
+    let mut f = File::create(format!("{}_merged.log", file_stem.to_str().unwrap()))?;
+    for line in base_log {
+        f.write(format!("[{}] ", line.priority).as_bytes())?;
+        f.write(line.content.as_bytes())?;
+        f.write(LINE_ENDING.as_bytes())?;
+    }
+    Ok(())
 }
